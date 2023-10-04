6f800b474b7f9c6e04a46234712fb589e32f9d19
SaiVeerendra9 <prathipati.s@northeastern.edu>
2023-10-03 22:23:53 -0400

    Fixed error in test file and added user.csv

--- spec/test/healthRouter_integration.test.js
+++ spec/test/healthRouter_integration.test.js
@@ -3,9 +3,8 @@ const request = require('supertest');
 const router = require('../../src/routes/healthRouter.js');
 
 const app = express();
-app.use('/', router);
+app.use('/healthz', router);
 
-console.log("ENV FILES", process.env);
 describe('Test Suite: Healthz Integration Test Cases', () => {   
     it('Test Case 1: GET API Case, when DB is connected', (done) => {
         request(app).get('/healthz')
